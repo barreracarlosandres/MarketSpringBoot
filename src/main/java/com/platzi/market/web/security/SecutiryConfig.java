17640163822624581f99ddda7a9bb1b4509a4cf2
Carlos Barrera <carlos.barrera@ceiba.com.co>
2022-05-03 13:34:25 -0500

    Se terminó la versión 1 del api market

--- src/main/java/com/platzi/market/web/security/SecutiryConfig.java
+++ src/main/java/com/platzi/market/web/security/SecutiryConfig.java
@@ -1,5 +1,6 @@
 package com.platzi.market.web.security;
 
+import com.platzi.market.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,13 +9,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import com.platzi.market.domain.service.MarketUserDatailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MarketUserDatailsService marketUserDatailsService;
+
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -24,8 +30,17 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //Sólo dejará sin autenticacion el método que genera el token
-        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
-                .anyRequest().authenticated();
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/**/authenticate").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
